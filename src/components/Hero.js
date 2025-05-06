215f2dd65a99f33d4b3347423329a50a938ca456
Vikramaditya Singh <vikramaditya.singh@move37capital.com>
2025-05-06 15:28:50 +0530

    change bg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,23 +17,23 @@ const Hero = () => {
       </video> */}
       <div className="absolute inset-0">
         <img
-          src="/images/hero5.jpg"
+          src="/images/image.png"
           alt="Background"
           className="w-full h-full object-cover opacity-100"
         />
       </div>
       {/* Overlay */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-60 z-10"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30 z-10"></div>
 
       {/* Hero content */}
       <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-white px-6">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">We are Fastest Growing Investment Company</h1>
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">We are Fast Growing Asset management firm</h1>
         <p className="text-lg md:text-2xl mb-6">
-          Trusted by top investors worldwide for excellence in portfolio management.
+          Trust by top institutional investors
         </p>
         <Link
           to="/contact"
-          className="bg-yellow-400 text-black px-6 py-3 rounded-full font-semibold hover:bg-yellow-300 transition"
+          className="bg-blue-500 text-black px-6 py-3 rounded-full font-semibold hover:bg-blue-700 transition"
         >
           Get in Touch
         </Link>
