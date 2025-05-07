96d14663a0aeeaadad8e29fe6ad41150f90f3211
Vikramaditya Singh <vikramaditya.singh@move37capital.com>
2025-05-07 11:55:09 +0530

    privacy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<AboutPage />} /> */}
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
         
       </Routes>
     </BrowserRouter>
