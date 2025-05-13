946173c5c4d0dc309ed31521cddbe7e933f9ebc0
Vikramaditya Singh <vikramaditya.singh@move37capital.com>
2025-05-13 12:08:09 +0530

    do changes font, navbar color

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,25 +13,24 @@ const Hero = () => {
         />
       </div>
 
-      {/* Optional dark overlay for contrast */}
+      {/* Optional overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-0 z-10"></div>
 
       {/* Hero content */}
-      <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-blue-900 px-4 transform-none sm:transform sm:translate-x-[-40px] sm:translate-y-[-40px]">
+      <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-blue-900 px-4 sm:px-6 md:px-8 lg:px-12 transform-none lg:transform lg:translate-x-[-62px] lg:translate-y-[-60px]">
 
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 leading-tight max-w-4xl">
-          A Fast Growing Asset Management Firm
-          <sup className="relative -top-3 sm:-top-4 md:-top-5 text-sm sm:text-base md:text-lg lg:text-xl mr-4">*</sup>
-
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-[28px] font-semibold mb-3 leading-tight max-w-4xl">
+        A Fast Growing Asset Management Firm
+          <sup className="relative -top-2 text-xs sm:text-sm ml-1">*</sup>
         </h1>
 
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-6 max-w-xl">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-5 max-w-xl whitespace-nowrap">
           Trusted by top institutional investors
         </p>
 
         <Link
           to="/contact"
-          className="bg-[#122F43] text-white px-6 py-3 rounded-full font-semibold hover:bg-[#122F68] transition text-sm sm:text-base"
+          className="bg-[#1b4460] text-white px-6 py-3 rounded-full font-medium hover:bg-[#122F43] transition text-sm sm:text-base"
         >
           Get in Touch
         </Link>
