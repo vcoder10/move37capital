ebe52fbfd6becaf65151c7f7e86b047b78267566
Vikramaditya Singh <vikramaditya.singh@move37capital.com>
2025-05-19 13:14:34 +0530

    mobile7

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,18 +18,18 @@ const Hero = () => {
 
       {/* Hero content */}
       <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-blue-900 px-4 sm:px-6 md:px-8 lg:px-12 lg:transform lg:translate-x-[-62px] lg:translate-y-[-60px]">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-[28px] font-semibold mb-3 leading-tight max-w-4xl landscape:text-base">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-[28px] font-semibold mb-3 leading-tight max-w-4xl mobile-landscape-heading:text-base">
           A Fast Growing Asset Management Firm
           <sup className="relative -top-2 text-xs sm:text-sm ml-1">*</sup>
         </h1>
 
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-5 max-w-xl whitespace-normal landscape:text-xs">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-5 max-w-xl whitespace-normal mobile-landscape-heading:text-xs">
           Trusted by top institutional investors
         </p>
 
         <Link
           to="/contact"
-          className="bg-[#1b4460] text-white px-4 py-2 rounded-full font-medium hover:bg-[#122F43] transition text-xs sm:text-sm landscape:text-xs"
+          className="bg-[#1b4460] text-white px-4 py-2 rounded-full font-medium hover:bg-[#122F43] transition text-xs sm:text-sm mobile-landscape-heading:text-xs"
         >
           Get in Touch
         </Link>
